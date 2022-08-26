46440fc6e1898a022b56a5bd9733d396923b4eaa
z30025928 <734222381@qq.com>
2022-08-26 16:48:40 +0800

    检视意见修改
    Signed-off-by: z30025928 <734222381@qq.com>

--- interfaces/inner_api/include/napi_util.h
+++ interfaces/inner_api/include/napi_util.h
@@ -87,18 +87,18 @@ void GetLocationArray(const napi_env& env, LocationMockAsyncContext *asyncContex
     } \
 }
 
-#define CHK_ERROR_CODE(type, errorCode, isRequired) \
-    do { \
-        if (errorCode == COMMON_ERROR || errorCode == INPUT_PARAMS_ERROR) { \
-            LBSLOGE(LOCATOR_STANDARD, "Js Object to other types failed."); \
-            return errorCode; \
-        } \
-        if (isRequired && errorCode == PARAM_IS_EMPTY) { \
-            LBSLOGE(LOCATOR_STANDARD, "The required ##%{public}s field should not be empty.", type); \
-            return INPUT_PARAMS_ERROR; \
-        } \
-    } while(0)
-
+#define CHK_ERROR_CODE(type, theCall, isRequired)                                                   \
+    do {                                                                                            \
+        int errorCode = (theCall);                                                                  \
+        if (errorCode == COMMON_ERROR || errorCode == INPUT_PARAMS_ERROR) {                         \
+            LBSLOGE(LOCATOR_STANDARD, "Js Object to other types failed.");                          \
+            return errorCode;                                                                       \
+        }                                                                                           \
+        if (isRequired && errorCode == PARAM_IS_EMPTY) {                                            \
+            LBSLOGE(LOCATOR_STANDARD, "The required #%{public}s field should not be empty.", type); \
+            return INPUT_PARAMS_ERROR;                                                              \
+        }                                                                                           \
+    } while (0)
 }  // namespace Location
 }  // namespace OHOS
 #endif // NAPI_UTIL_H
