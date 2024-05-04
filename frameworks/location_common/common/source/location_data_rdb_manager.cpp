00421dd51f6006d439837beb13dffd7bb61cc136
ruan-shaofei <ruanshaofei2020@163.com>
2024-05-04 16:56:16 +0800

    add location switch mode cache mechanism
    Signed-off-by: ruan-shaofei <ruanshaofei2020@163.com>

--- frameworks/location_common/common/source/location_data_rdb_manager.cpp
+++ frameworks/location_common/common/source/location_data_rdb_manager.cpp
@@ -16,11 +16,14 @@
 
 #include "common_utils.h"
 #include "location_data_rdb_helper.h"
-
+#include "parameter.h"
 
 namespace OHOS {
 namespace Location {
 const int DEFAULT_USERID = 100;
+const int UNKNOW_ERROR = -1;
+const int MAX_SIZE = 100;
+const char* LOCATION_SWITCH_MODE = "persist.location.switch_mode";
 
 std::string LocationDataRdbManager::GetLocationDataUri(std::string key)
 {
@@ -72,6 +75,37 @@ bool LocationDataRdbManager::GetLocationWorkingState(int32_t& state)
         GetValue(locationWorkingStateUri, LOCATION_WORKING_STATE, state);
     if (errCode != ERRCODE_SUCCESS) {
         LBSLOGE(COMMON_UTILS, "%{public}s: can not get value, errcode = %{public}d", __func__, errCode);
+        return false;
+    }
+    return true;
+}
+
+int LocationDataRdbManager::GetSwitchMode()
+{
+    char result[MAX_SIZE] = {0};
+    std::string value = "";
+    auto res = GetParameter(LOCATION_SWITCH_MODE, "", result, MAX_SIZE);
+    if (res <= 0) {
+        LBSLOGE(COMMON_UTILS, "%{public}s get para value failed, res: %{public}d", __func__, res);
+        return UNKNOW_ERROR;
+    }
+    value = result;
+    for (auto ch : value) {
+        if (std::isdigit(ch) == 0) {
+            LBSLOGE(COMMON_UTILS, "wrong para");
+            return UNKNOW_ERROR;
+        }
+    }
+    return std::stoi(value);
+}
+
+bool LocationDataRdbManager::SetSwitchMode(int value)
+{
+    char valueArray[MAX_SIZE] = {0};
+    (void)sprintf_s(valueArray, sizeof(valueArray), "%d", value);
+    int res = SetParameter(LOCATION_SWITCH_MODE, valueArray);
+    if (res < 0) {
+        LBSLOGE(COMMON_UTILS, "%{public}s failed, res: %{public}d", __func__, res);
         return false;
     }
     return true;
