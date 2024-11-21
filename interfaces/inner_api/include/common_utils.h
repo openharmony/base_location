b0fc8ce38652b42661fbfe987d6b78770f065699
smilebear <245252081@qq.com>
2024-11-21 06:12:29 +0000

    update interfaces/inner_api/include/common_utils.h.
    Signed-off-by: smilebear <245252081@qq.com>

--- interfaces/inner_api/include/common_utils.h
+++ interfaces/inner_api/include/common_utils.h
@@ -179,6 +179,7 @@ public:
     static bool GetStringParameter(const std::string& type, std::string& value);
     static bool GetEdmPolicy(std::string& name);
     static std::string GenerateUuid();
+    static bool CheckAppForUser(int32_t uid, std::string& bundleName);
     static bool CheckAppForUser(int32_t uid, int32_t currentUserId, std::string& bundleName);
     static int64_t GetSinceBootTime();
     static bool GetAllUserId(std::vector<int>& activeIds);
