e2111decb320f9b8643938266cd19e630c5f9dde
smilebear <245252081@qq.com>
2024-12-20 13:44:49 +0000

    update services/location_network/network/source/network_ability.cpp.
    Signed-off-by: smilebear <245252081@qq.com>

--- services/location_network/network/source/network_ability.cpp
+++ services/location_network/network/source/network_ability.cpp
@@ -221,9 +221,6 @@ void NetworkAbility::NotifyConnected(const sptr<IRemoteObject>& remoteObject)
 
 void NetworkAbility::NotifyDisConnected()
 {
-    std::unique_lock<ffrt::mutex> uniqueLock(nlpServiceMutex_);
-    nlpServiceProxy_ = nullptr;
-    connectCondition_.notify_all();
 }
 
 LocationErrCode NetworkAbility::SendLocationRequest(WorkRecord &workrecord)
