dea9c29062cbf78908fa10a39aa3b9f13d67cedc
liuxinbing <liuxinbing4@huawei.com>
2024-12-23 15:52:59 +0800

    bugfix
    Signed-off-by: liuxinbing <liuxinbing4@huawei.com>

--- services/location_locator/locator/source/report_manager.cpp
+++ services/location_locator/locator/source/report_manager.cpp
@@ -224,13 +224,13 @@ std::unique_ptr<Location> ReportManager::GetPermittedLocation(const std::shared_
     auto firstTokenId = request->GetFirstTokenId();
     auto tokenIdEx = request->GetTokenIdEx();
     auto uid =  request->GetUid();
-    auto pid =  request->GetPid();
     if (bundleName.length() == 0) {
         if (!CommonUtils::GetBundleNameByUid(uid, bundleName)) {
             LBSLOGD(REPORT_MANAGER, "Fail to Get bundle name: uid = %{public}d.", uid);
         }
     }
-    if (IsAppBackground(bundleName, tokenId, tokenIdEx, uid, pid) &&
+    if (request->GetRequestConfig()->GetFixNumber() == 0 &&
+        IsAppBackground(bundleName, tokenId, tokenIdEx, uid, request->GetPid()) &&
         !PermissionManager::CheckBackgroundPermission(tokenId, firstTokenId)) {
         //app background, no background permission, not ContinuousTasks
         RequestManager::GetInstance()->ReportLocationError(LOCATING_FAILED_BACKGROUND_PERMISSION_DENIED, request);
