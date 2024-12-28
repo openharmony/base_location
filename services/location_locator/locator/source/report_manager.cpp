63c5b7232c251debc107ff84e94b9d8ab456a2aa
liuxinbing <liuxinbing4@huawei.com>
2024-12-28 16:25:23 +0800

    bugfix
    Signed-off-by: liuxinbing <liuxinbing4@huawei.com>

--- services/location_locator/locator/source/report_manager.cpp
+++ services/location_locator/locator/source/report_manager.cpp
@@ -162,9 +162,9 @@ bool ReportManager::ProcessRequestForReport(std::shared_ptr<Request>& request,
             return false;
         }
         LBSLOGW(REPORT_MANAGER, "report location to %{public}d, uuid : %{public}s, " \
-            "TimeSinceBoot : %{public}lld, SourceType : %{public}d",
+            "TimeSinceBoot : %{public}s, SourceType : %{public}d",
             request->GetTokenId(), request->GetUuid().c_str(),
-            finalLocation->GetTimeSinceBoot(), finalLocation->GetLocationSourceType());
+            std::to_string(finalLocation->GetTimeSinceBoot()).c_str(), finalLocation->GetLocationSourceType());
         locatorCallback->OnLocationReport(finalLocation);
         RequestManager::GetInstance()->UpdateLocationError(request);
     }
