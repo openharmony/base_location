68d716f472ec2df89c381aeecd8a62d5bc4b7d35
yihangYou <youyihang13@163.com>
2025-02-06 16:54:02 +0800

    bugfix
    Signed-off-by: yihangYou <youyihang13@163.com>

--- frameworks/js/napi/source/location_napi_errcode.cpp
+++ frameworks/js/napi/source/location_napi_errcode.cpp
@@ -34,11 +34,12 @@ void ThrowBusinessError(const napi_env &env, int32_t errCode)
 {
     LBSLOGI(LOCATOR_STANDARD, "ThrowBusinessError, errCode = %{public}d", errCode);
     std::string errMsg = GetErrorMsgByCode(errCode);
+    if (errMsg == "") {
+        return;
+    }
     errCode = ConvertErrorCode(errCode);
     auto businessError = CreateError(env, errCode, errMsg);
-    if (errMsg != "") {
-        napi_throw(env, businessError);
-    }
+    napi_throw(env, businessError);
 }
 }  // namespace Location
 }  // namespace OHOS
