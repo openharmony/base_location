9276d9e5207e62109c3a686ef29eb1a33327a34e
wen_lung_lee <liwenlong22@huawei.com>
2025-11-07 16:49:10 +0800

    codeclean
    Signed-off-by: wen_lung_lee <liwenlong22@huawei.com>

--- services/location_gnss/gnss/source/gnss_common_event_subscriber.cpp
+++ services/location_gnss/gnss/source/gnss_common_event_subscriber.cpp
@@ -21,7 +21,6 @@
 
 namespace OHOS {
 namespace Location {
-static constexpr int32_t DATA_MSG_RECEIVE_CODE = 1;
 void GnssCommonEventSubscriber::OnReceiveEvent(const OHOS::EventFwk::CommonEventData &data)
 {
     LBSLOGD(GNSS, "OnReceiveEvent");
@@ -29,7 +28,7 @@ void GnssCommonEventSubscriber::OnReceiveEvent(const OHOS::EventFwk::CommonEvent
     std::string action = want.GetAction();
 
     auto agnssNiManager = AGnssNiManager::GetInstance();
-    if (action == CommonEventSupport::COMMON_EVENT_SMS_RECEIVE_COMPLETED && data.GetCode() == DATA_MSG_RECEIVE_CODE) {
+    if (action == CommonEventSupport::COMMON_EVENT_SMS_RECEIVE_COMPLETED) {
         LBSLOGI(GNSS, "[AGNSS NI]:receive sms data msg");
         agnssNiManager->CheckSmsSuplInit(want);
     } else if (action == CommonEventSupport::COMMON_EVENT_SMS_WAPPUSH_RECEIVE_COMPLETED) {
