2b8da4443ce06b11d5d031bb5d6c6ea1815e704e
smilebear <yourongrong1@h-partners.com>
2025-12-31 09:53:31 +0800

    优化前台多用户的逻辑
    Signed-off-by: smilebear <yourongrong1@h-partners.com>

--- frameworks/location_common/common/source/location_data_rdb_manager.cpp
+++ frameworks/location_common/common/source/location_data_rdb_manager.cpp
@@ -167,7 +167,7 @@ bool LocationDataRdbManager::SetGnssSessionState(int32_t state, std::string uri,
     return true;
 }
 
-bool LocationDataRdbManager::GetSwitchStateFrrUser(int userId)
+bool LocationDataRdbManager::GetSwitchStateForUser(int userId)
 {
     return GetSwitchStateFromSysparaForUser(userId);
 }
