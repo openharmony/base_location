a523fc8c3487763993df8da702038a7c8a06f80c
sidecai <sidecai@h-partners.com>
2026-06-27 11:54:35 +0800

    codecheck
    Signed-off-by: sidecai <sidecai@h-partners.com>

--- frameworks/native/geofence_sdk/source/geofence_request.cpp
+++ frameworks/native/geofence_sdk/source/geofence_request.cpp
@@ -529,16 +529,16 @@ void GeofenceRequest::ConvertGeofenceRequestInfoExt(std::shared_ptr<GeofenceRequ
     const nlohmann::json &jsonObject)
 {
     if (jsonObject.find("pid") != jsonObject.cend() && jsonObject.at("pid").is_number()) {
-        request->pid_ = jsonObject.at("pid").get<int>();
+        request->pid_ = jsonObject.at("pid").get<int32_t>();
     }
     if (jsonObject.find("tokenId") != jsonObject.cend() && jsonObject.at("tokenId").is_number()) {
-        request->tokenId_ = jsonObject.at("tokenId").get<int>();
+        request->tokenId_ = jsonObject.at("tokenId").get<uint32_t>();
     }
     if (jsonObject.find("tokenIdEx") != jsonObject.cend() && jsonObject.at("tokenIdEx").is_number()) {
-        request->tokenIdEx_ = jsonObject.at("tokenIdEx").get<int>();
+        request->tokenIdEx_ = jsonObject.at("tokenIdEx").get<uint32_t>();
     }
     if (jsonObject.find("firstTokenId") != jsonObject.cend() && jsonObject.at("firstTokenId").is_number()) {
-        request->firstTokenId_ = jsonObject.at("firstTokenId").get<int>();
+        request->firstTokenId_ = jsonObject.at("firstTokenId").get<uint32_t>();
     }
 }
 
