90908efbeef065703ab397a0a607e8b9236f04d4
xxthadsl <wangtianhe8@huawei.com>
2026-09-07 17:10:16 +0800

    fix nullptr
    Signed-off-by: xxthadsl <wangtianhe8@huawei.com>

--- services/location_locator/locator/source/fusion_controller.cpp
+++ services/location_locator/locator/source/fusion_controller.cpp
@@ -101,6 +101,9 @@ std::unique_ptr<Location> FusionController::GetFuseLocation(const std::unique_pt
     const sptr<Location>& lastFuseLocation)
 {
     LBSLOGD(FUSION_CONTROLLER, " GetFuseLocation enter");
+    if (lastFuseLocation == nullptr) {
+        return nullptr;
+    }
     auto bestLocation = chooseBestLocation(location, std::make_unique<Location>(*lastFuseLocation));
     if (bestLocation == nullptr) {
         return nullptr;
