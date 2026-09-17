249b41bfb2d81001a78d427a40b30b2ce10a62f0
xxthadsl <wangtianhe8@huawei.com>
2026-09-17 14:27:12 +0800

    whitelist
    Signed-off-by: xxthadsl <wangtianhe8@huawei.com>

--- services/location_locator/locator/source/locator_ability.cpp
+++ services/location_locator/locator/source/locator_ability.cpp
@@ -570,6 +570,11 @@ ErrCode LocatorAbility::EnableAbility(bool isEnabled)
         LBSLOGE(LOCATOR, "OpenPrivacyDialog");
         return ERRCODE_SERVICE_UNAVAILABLE;
     }
+    if (!HookUtils::ExecuteHookEnableAbility(
+        identity.GetBundleName().size() == 0 ? std::to_string(identity.GetUid()) : identity.GetBundleName(),
+        isEnabled, userId, identity.GetTokenId())) {
+        return ERRCODE_SUCCESS;
+    }
     LocationErrCode errCode = SetSwitchStateForUser(isEnabled, userId, identity.GetBundleName());
     std::string bundleName;
     bool result = LocationConfigManager::GetInstance()->GetSettingsBundleName(bundleName);
@@ -606,11 +611,6 @@ ErrCode LocatorAbility::EnableAbilityForUser(bool isEnabled, int32_t userId)
         LocationConfigManager::GetInstance()->OpenPrivacyDialog();
         LBSLOGE(LOCATOR, "OpenPrivacyDialog");
         return ERRCODE_SERVICE_UNAVAILABLE;
-    }
-    if (!HookUtils::ExecuteHookEnableAbility(
-        identity.GetBundleName().size() == 0 ? std::to_string(identity.GetUid()) : identity.GetBundleName(),
-        isEnabled, userId, identity.GetTokenId())) {
-        return ERRCODE_SUCCESS;
     }
     if (!HookUtils::ExecuteHookEnableAbility(
         identity.GetBundleName().size() == 0 ? std::to_string(identity.GetUid()) : identity.GetBundleName(),
