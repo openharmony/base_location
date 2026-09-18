1a79e6736e2336b603eff0959d8aad382460fb20
xxthadsl <wangtianhe8@huawei.com>
2026-09-18 14:19:25 +0800

    whitelist
    Signed-off-by: xxthadsl <wangtianhe8@huawei.com>

--- services/location_locator/locator/source/locator_ability.cpp
+++ services/location_locator/locator/source/locator_ability.cpp
@@ -1541,11 +1541,6 @@ ErrCode LocatorAbility::StartLocating(const RequestConfig& requestConfig, const 
 {
     AppIdentity identity;
     GetAppIdentityInfo(identity);
-    if (!CheckRequestAvailable(LocatorInterfaceCode::START_LOCATING, identity)) {
-        WriteLocationInnerEvent(LBS_REQUEST_FAIL_DETAIL, {"REQ_APP_NAME", identity.GetBundleName(),
-                "NETWORK_FAIL_CODE", std::to_string(LOCATION_ERRCODE_NOT_CURRENT_USER_ID)});
-        return LOCATION_ERRCODE_PERMISSION_DENIED;
-    }
     if (!GetLocationSwitchIgnoredFlag(identity.GetTokenId()) && !CheckLocationSwitchState()) {
         WriteLocationInnerEvent(LBS_REQUEST_FAIL_DETAIL, {"REQ_APP_NAME", identity.GetBundleName(),
                 "NETWORK_FAIL_CODE", std::to_string(ERRCODE_SWITCH_OFF)});
